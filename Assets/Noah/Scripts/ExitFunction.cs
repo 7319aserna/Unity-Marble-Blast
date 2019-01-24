55ce417615510579e97179448777e3f6d8ecb29b
Noah Albright <albright.noahk@gmail.com>
2019-01-24 12:04:51 -0800

    Added exit code with basic funtionality

---
+++ Assets/Noah/Scripts/ExitFunction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitFunction : MonoBehaviour {
+    Scene nextLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+            other.GetComponent<BallController>().enabled = false;
+            GetComponent<ExitMovement>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Noah/Scripts/ExitMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitMovement : MonoBehaviour {
+
+    public Transform player;
+    public float forceMult;
+    private Vector3 dir;
+	
+	// Update is called once per frame
+	void Update () {
+        dir = GetComponentInParent<Transform>().position + transform.position - player.position;
+        player.GetComponent<Rigidbody>().AddForce(dir * forceMult);
+	}
+}
