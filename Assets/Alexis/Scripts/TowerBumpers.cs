9b04487bcb93bf93750e15dd7e70642b32533e92
Marcel Dao <marcelnamdao@gmail.com>
2019-01-24 09:35:16 -0800

    disable automatic lightmap generation

---
+++ Assets/Alexis/Scripts/TowerBumpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBumpers : MonoBehaviour {
+    [SerializeField]
+    Vector3 bumperOffset;
+    float bumperTimerChange = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = bumperOffset;
+    }
+	// Update is called once per frame
+	void Update () {
+        bumperTimerChange += Time.deltaTime;
+        transform.position = new Vector3(bumperOffset.x, Random.Range(-25.0f, 25.0f), bumperOffset.z);
+    }
+}
