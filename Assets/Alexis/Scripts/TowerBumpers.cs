45b1991f9aaf94509ca09689a20cc398d97f90d9
Alexis Serna <s189074@students.aie.edu>
2019-01-24 16:35:50 -0800

    Implemented a Menu Screen, along with instructions

--- Assets/Alexis/Scripts/TowerBumpers.cs
+++ Assets/Alexis/Scripts/TowerBumpers.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TowerBumpers : MonoBehaviour {
     [SerializeField]
     Vector3 bumperOffset;
-    float bumperTimerChange = 0.0f;
 
     // Use this for initialization
     void Start()
@@ -14,7 +13,6 @@ public class TowerBumpers : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-        bumperTimerChange += Time.deltaTime;
         transform.position = new Vector3(bumperOffset.x, Random.Range(-25.0f, 25.0f), bumperOffset.z);
     }
 }
